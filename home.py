dac875275ff0674525aea3e2deca86cd0161f7c4
user_aditia <user_aditia01101999@gmail.com>
2024-05-14 14:48:47 +0700

    push

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Profile')
+st.subheader('Nama : ADITIA SEFTIAWAN')
+st.subheader('NIM : 312210094')
+st.subheader('Kelas : TI.22.B1')
+st.subheader('Prodi : Teknik Infomatika')
+
+st.image("./img/PROFILE_ADIT.jpg", caption="ADIT", use_column_width="Always", width=200)
+
+
+st.link_button("Instagram", "https://www.instagram.com/adt_set01/")
+st.link_button("Github", "https://github.com/aditia0110")
